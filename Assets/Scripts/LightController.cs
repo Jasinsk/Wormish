85a888ede8fa0ab5a97edd0491a6a6540b23a27d
Jasinsk <jaisnski.ja@gmail.com>
2019-05-18 14:45:16 +0200

    Added: Day Night cycle visual alpha version
    their is a day night cycle but it is a prototype. I still have to add light disableing and certain optimazation changes. Tempo is so high to easily see the effect.
    The game doesn't change yet. This is only visual

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+    public float rotationAngle = 1;
+
+    void Start()
+    {
+        m_rotator = new Vector3(0, rotationAngle, 0);
+        m_light = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        transform.Rotate(m_rotator, Space.Self);
+        //if (transform.rotation.y > 90 && transform.rotation.y < 270)
+        //{
+        //    m_light.enabled = false;
+        //}
+        //else
+        //{
+        //    m_light.enabled = true;
+        //}
+        //Debug.Log(transform.rotation.y);
+        
+    }
+
+    private Vector3 m_rotator;
+    private Light m_light;
+}
