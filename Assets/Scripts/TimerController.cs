a8369ae0eef33885e34eb7f7ce6b71cd7e3e556f
Jasinsk <jasinski.ja@gmail.com>
2019-05-26 15:16:06 +0200

    Added: Visual timer representation - alpha version

--- Assets/Scripts/TimerController.cs
+++ Assets/Scripts/TimerController.cs
@@ -4,17 +4,27 @@ using UnityEngine;
 
 public class TimerController : MonoBehaviour
 {
+    public GameObject ProgressIndicator;
     void Start()
     {
         settings = GameObject.FindGameObjectWithTag("LevelSettings").GetComponent<LevelSettings>();
         m_gameController = GetComponentInParent<GameController>();
         m_TimeLeft = settings.GetLevelDuration();
+        m_indicatorTransform = ProgressIndicator.GetComponent<RectTransform>();
+        m_maxScale = m_indicatorTransform.localScale.x;
+
+        m_indicatorTransform.localScale = new Vector3(0, 0, 1);
+
+
+
     }
     void Update()
     {
         if (m_gameController.GetGameRunning())
         {
             m_TimeLeft -= Time.deltaTime;
+            m_scaler = m_maxScale * ((settings.GetLevelDuration() - m_TimeLeft) / settings.GetLevelDuration());
+            m_indicatorTransform.localScale = new Vector3(m_scaler, m_scaler, 1);
             if (m_TimeLeft < 0)
             {
                 m_gameController.EndLevel();
@@ -23,6 +33,7 @@ public class TimerController : MonoBehaviour
     }
 
     private LevelSettings settings;
-    private float m_TimeLeft;
+    private float m_TimeLeft, m_maxScale, m_scaler;
+    private RectTransform m_indicatorTransform;
     private GameController m_gameController;
 }
