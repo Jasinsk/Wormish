a5ac21cee58c69264f8dce25468c63e076b76714
Jasinsk <jasinski.ja@gmail.com>
2019-05-27 23:21:47 +0200

    Changed: Complete menu redesign

---
+++ Assets/Scripts/WormLogoController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is responsible for the menu screen worm animation
+public class WormLogoController : MonoBehaviour
+{
+    public float wormScaleMin = 0.7f;
+    public float wormScaleMax = 1.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (m_enlarging)
+        {
+            if (transform.localScale.y < wormScaleMax)
+            {
+                transform.localScale += new Vector3(-0.002f, 0.02f, 0);
+            }
+            else
+            {
+                m_enlarging = false;
+            }
+        }
+        else
+        {
+            if (transform.localScale.y > wormScaleMin)
+            {
+                transform.localScale += new Vector3(0.002f, -0.02f, 0);
+            }
+            else
+            {
+                m_enlarging = true;
+            }
+        }
+    }
+
+    private bool m_enlarging = true;
+}
